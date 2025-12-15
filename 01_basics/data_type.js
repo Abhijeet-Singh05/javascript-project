39f8752a292f7a11467903ffbbb3d330e217e8c1
Abhijeet-Singh05 <singh.abhijeet9342@gmail.com>
2025-12-15 15:48:56 +0000

    basics done

---
+++ 01_basics/data_type.js
@@ -0,0 +1,46 @@
+// Stack (primitive)  Heap (Non primitive)
+/*
+let someName = "ibha"
+let anotherName = someName
+
+console.log(someName)
+console.log(anotherName)
+
+// anotherName = "jeet"
+someName = "jeet"
+
+console.log(anotherName)
+console.log(someName)
+*/
+
+// userOne = {
+//     "name":"ibha",
+//     "email":"ibha@gmail.com"
+// }
+
+// userTwo = userOne
+
+// console.log(userOne.name)
+// console.log(userTwo.name)
+
+// userTwo.name = "jeet"
+
+// console.log(userOne.name)
+// console.log(userTwo.name)
+
+// const gameName = "ihbateej"
+
+// console.log(gameName.length)
+// console.log(gameName.slice(0,4))
+// console.log(gameName.substring(0,4))
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myDate.getMonth())
+console.log(`current month is ${myDate.getMonth() + 1}`)
