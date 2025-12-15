7b295e45389bf4b8d3fdcda99b76de869bca643e
Abhijeet-Singh05 <singh.abhijeet9342@gmail.com>
2025-12-15 17:11:54 +0000

    arrays

---
+++ arrays/array.1.js
@@ -0,0 +1,44 @@
+// array
+
+// const arr = [1,2,3,4]
+// console.log(arr[0])
+// console.log(arr[-1])
+//console.log(arr.length)
+
+// arr.push(5)
+//console.log(arr)
+
+// arr.unshift(0)
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+
+//console.log(arr.indexOf(0))
+
+// console.log(arr.slice(1,4))
+
+// console.log(arr.join())
+
+// console.log(arr)
+
+// console.log(arr.splice(1,4))
+
+// console.log(arr)
+
+
+// const marvel_heroes = ["thor","ironman","captain america"]
+// const dc_heroes = ["superman","batman","flash"]
+
+// marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes)
+
+// marvel_heroes.concat(dc_heroes) // not update, but return 
+// console.log(marvel_heroes.concat(dc_heroes))
+
+// const all_heroes = [...marvel_heroes, ...dc_heroes]
+// console.log(all_heroes)
+
+console.log(Array.isArray("ihba"))
+console.log(Array.from("ihba"))
+console.log(Array.of({name:"ihba"}))
