a290eddf04d80b53e8449a51d2346d4606528ec3
Abhijeet-Singh05 <singh.abhijeet9342@gmail.com>
2025-12-18 14:03:53 +0000

    for loop

---
+++ iterations/for.js
@@ -0,0 +1,13 @@
+// for
+
+// for(let index = 0; index < 10; index++ ){
+//     const ele = index
+//     console.log(ele)
+// }
+// let x = 0
+// for(let i=0; i<10; i++){
+//     for(let j=0; j<i; j++){
+//         console.log(`${i} x ${j}`)
+//         x++
+//     }
+// }
